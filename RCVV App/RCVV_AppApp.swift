d4f3a28684eac5f05a7d4363da343641fa586578
Daniel Figueroa <danfigueroa81@gmail.com>
2023-01-13 11:40:43 -0500

    Initial Commit

---
+++ RCVV App/RCVV_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCVV_AppApp.swift
+//  RCVV App
+//
+//  Created by Daniel Figueroa on 2023-01-13.
+//
+
+import SwiftUI
+
+@main
+struct RCVV_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
